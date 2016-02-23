b00e2407fb6fc6957e1cd13701c2df6ce800b9be
Dhia-chan <ardhia>
2016-02-23 15:51:33 +0700

    menu test di delete

--- index.php
+++ index.php
@@ -27,7 +27,6 @@
 					<li><a href="logUp.php">Log Up</a></li>
 					<li><a href="logIn.php">Log In</a></li>
 					<li><a href="logOut.php">Log Out</a></li>
-					<li><a href="logOut.php">test</a></li>
 				</ul>
 			</div>
 		</div>
