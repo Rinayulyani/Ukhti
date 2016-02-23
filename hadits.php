d343d355ea1163a68471e468da95d6e7d05659c1
Shania14 <shania.indriani14@gmail.com>
2016-02-23 17:09:54 +0700

    sudah dirubah, dll,hadits,pakaian

--- hadits.php
+++ hadits.php
@@ -5,114 +5,116 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	<div id="header">
-		<div class="log">
-			<a href="index.php"><div id="logo">Ukhti</div></a>
-			<div id="log">
+	<div class="all-wrapper">
+		<div id="header">
+			<div class="log">
+				<a href="index.php"><div id="logo">Ukhti</div></a>
+				<div id="log">
+					<ul>
+						<li><a href="about.php">About</a></li>
+						<li><a href="logUp.php">Log Up</a></li>
+						<li><a href="logIn.php">Log In</a></li>
+						<li><a href="logOut.php">Log Out</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="nav">
 				<ul>
-					<li><a href="about.php">About</a></li>
-					<li><a href="logUp.php">Log Up</a></li>
-					<li><a href="logIn.php">Log In</a></li>
-					<li><a href="logOut.php">Log Out</a></li>
+					<div class="nav-in"><li><a href="jubel.php">Toko</a></li></div>
+					<div class="nav-in"><li><a href="kisah.php">Kisah</a></li></div>
+					<div class="nav-in"><li><a href="tutorial.php">Tutorial</a></li></div>
+					<div class="nav-in"><li><a href="hadits.php">Hadits</a></li></div>
 				</ul>
 			</div>
 		</div>
-		<div class="nav">
-			<ul>
-				<div class="nav-in"><li><a href="jubel.php">Toko</a></li></div>
-				<div class="nav-in"><li><a href="kisah.php">Kisah</a></li></div>
-				<div class="nav-in"><li><a href="tutorial.php">Tutorial</a></li></div>
-				<div class="nav-in"><li><a href="hadits.php">Hadits</a></li></div>
-			</ul>
+		<div id="search">
+			<form>
+				<input type="text" name="" placeholder="Search"></input>
+				<input type="submit" name="search" value=""></input>
+			</form>
 		</div>
-	</div>
-	<div id="search">
-		<form>
-			<input type="text" name="" placeholder="Search"></input>
-			<input type="submit" name="search" value=""></input>
-		</form>
-	</div>
-	<div id="wrapper">
-		<div class="wrap">
-			<div class="content">
-				<div id="artikel">
-					<div class="judul-artikel">
-						HR. Muslim no. 1467
+		<div id="wrapper">
+			<div class="wrap">
+				<div class="content">
+					<div id="artikel">
+						<div class="judul-artikel">
+							HR. Muslim no. 1467
+						</div>
+						<div class="ringkasan-artikel">
+							Abdullah bin Amr radhiyallahu'anhuma meriwayatkan sabda Rasulullah Shalallahu'alaihi wa sallam : "Sesungguhnya dunia itu adalah perhiasan dan sebaik-baik perhiasan dunia adalah wanita shalihah."
+						</div>
 					</div>
-					<div class="ringkasan-artikel">
-						Abdullah bin Amr radhiyallahu'anhuma meriwayatkan sabda Rasulullah Shalallahu'alaihi wa sallam : "Sesungguhnya dunia itu adalah perhiasan dan sebaik-baik perhiasan dunia adalah wanita shalihah."
+					<div id="artikel">
+						<div class="judul-artikel">
+							HR. Al-Bukhari no. 5090 dan Muslim no. 1466
+						</div>
+						<div class="ringkasan-artikel">
+							Rasulullah Shalallahu'alaihi wa sallam bersabda bagi lelaki yang ingin menikah : "Wanita itu dinikahi karena empat perkara yaitu karena hartanya, karena keturunannya, karena kecantikannya, dan karena agamanya. Maka pilihlah olehmu wanita yang punya agama, engkau akan beruntung."
+						</div>
 					</div>
-				</div>
-				<div id="artikel">
-					<div class="judul-artikel">
-						HR. Al-Bukhari no. 5090 dan Muslim no. 1466
+					<div id="artikel">
+						<div class="judul-artikel">
+							HR. Bukhari dan Muslim
+						</div>
+						<div class="ringkasan-artikel">
+							Al Bukhari meriwayatkan dari Abu Hurairah radhiyallahu'anhu dari Nabi Shallallhu'alaihi wa sallam, beliau bersabda, yang artinya :
+							Barangsiapa yang beriman kepada Allah dan hari Akhir, janganlah ia mengganggu tetangganya, dan berbuat baiklah kepada wanita. Sebab, mereka diciptakan dari tulang rusuk, dan tulang rusuk yang paling bengkok adalah bagian atasnya. Jika engkau meluruskannya, maka engkau mematahkannya dan jika engkau biarkan, maka akan tetap bengkok. Oleh karena itu, berbuatlah baik kepada wanita."
+						</div>
 					</div>
-					<div class="ringkasan-artikel">
-						Rasulullah Shalallahu'alaihi wa sallam bersabda bagi lelaki yang ingin menikah : "Wanita itu dinikahi karena empat perkara yaitu karena hartanya, karena keturunannya, karena kecantikannya, dan karena agamanya. Maka pilihlah olehmu wanita yang punya agama, engkau akan beruntung."
+					<div id="artikel">
+						<div class="judul-artikel">
+							HR. Ahmad 1/191, dishahihkan Asy-Syaikh Al Albani rahimahullah dalam Shahihul Jami' no. 660, 661
+						</div>
+						<div class="ringkasan-artikel">
+							Rasulullah Shalallahu'alaihi wa sallam bersabda : "Apabila seorang wanita shalat lima waktu, puasa sebulan (Ramadhan), menjaga kemaluannya dan taat kepada suaminya, maka dikatakan kepadanya : Masuklah engkau ke dalam surga dari pintu mana saja yang engkau sukai."
+						</div>
 					</div>
-				</div>
-				<div id="artikel">
-					<div class="judul-artikel">
-						HR. Bukhari dan Muslim
-					</div>
-					<div class="ringkasan-artikel">
-						Al Bukhari meriwayatkan dari Abu Hurairah radhiyallahu'anhu dari Nabi Shallallhu'alaihi wa sallam, beliau bersabda, yang artinya :
-						Barangsiapa yang beriman kepada Allah dan hari Akhir, janganlah ia mengganggu tetangganya, dan berbuat baiklah kepada wanita. Sebab, mereka diciptakan dari tulang rusuk, dan tulang rusuk yang paling bengkok adalah bagian atasnya. Jika engkau meluruskannya, maka engkau mematahkannya dan jika engkau biarkan, maka akan tetap bengkok. Oleh karena itu, berbuatlah baik kepada wanita."
-					</div>
-				</div>
-				<div id="artikel">
-					<div class="judul-artikel">
-						HR. Ahmad 1/191, dishahihkan Asy-Syaikh Al Albani rahimahullah dalam Shahihul Jami' no. 660, 661
-					</div>
-					<div class="ringkasan-artikel">
-						Rasulullah Shalallahu'alaihi wa sallam bersabda : "Apabila seorang wanita shalat lima waktu, puasa sebulan (Ramadhan), menjaga kemaluannya dan taat kepada suaminya, maka dikatakan kepadanya : Masuklah engkau ke dalam surga dari pintu mana saja yang engkau sukai."
-					</div>
-				</div>
-				<div id="artikel">
-					<div class="judul-artikel">
-						HR. An-Nasaii ibnu Khuzaimah dan ibnu Hibban
-					</div>
-					<div class="ringkasan-artikel">
-						Rasulullah shalallahu'alaihi wa sallam bersabda : "Siapa saja wanita yang memakai wangi-wangian kemudian melewati suatu kaum supaya mereka itu mencium baunya, maka wanita itu telah dianggap melakukan zina dan tiap-tiap mata ada zina."
+					<div id="artikel">
+						<div class="judul-artikel">
+							HR. An-Nasaii ibnu Khuzaimah dan ibnu Hibban
+						</div>
+						<div class="ringkasan-artikel">
+							Rasulullah shalallahu'alaihi wa sallam bersabda : "Siapa saja wanita yang memakai wangi-wangian kemudian melewati suatu kaum supaya mereka itu mencium baunya, maka wanita itu telah dianggap melakukan zina dan tiap-tiap mata ada zina."
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="rightKet">
-				<div id="arsip">
-					<div class="judul">Arsip</div>
-					<div class="isi-arsip">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<div class="rightKet">
+					<div id="arsip">
+						<div class="judul">Arsip</div>
+						<div class="isi-arsip">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div id="footer">
-		<div class="contact">
-			<div id="logoF">Ukhti</div>
-			<div id="kontak">Facebook</div>
-			<div id="kontak">Twitter</div>
-			<div id="kontak">E-mail</div>
-			<div id="kontak">No. Kontak</div>
-			<div id="copyright">Ukhti &copy 2016</div>
-		</div>
-		<div class="info">
-			<div id="judul">Partner</div>
-			<div id="out-logo">
-				<div class="logoP"></div>
-				<div class="logoP"></div>
-				<div class="logoP"></div>
-				<div class="logoP"></div>
-				<div class="logoP"></div>
-				<div class="logoP"></div>
-				<div class="logoP"></div>
-				<div class="logoP"></div>
+		<div id="footer">
+			<div class="contact">
+				<div id="logoF">Ukhti</div>
+				<div id="kontak">Facebook</div>
+				<div id="kontak">Twitter</div>
+				<div id="kontak">E-mail</div>
+				<div id="kontak">No. Kontak</div>
+				<div id="copyright">Ukhti &copy 2016</div>
 			</div>
-		</div>
-		<div class="info">
-			<div id="judul">About Ukhti</div>
-			<div id="aboutringkasan">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			<div class="info">
+				<div id="judul">Partner</div>
+				<div id="out-logo">
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+					<div class="logoP"></div>
+				</div>
+			</div>
+			<div class="info">
+				<div id="judul">About Ukhti</div>
+				<div id="aboutringkasan">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
 			</div>
 		</div>
 	</div>
--- pakaian.php
+++ pakaian.php
@@ -42,33 +42,17 @@
 		</form>
 	</div>
 	<div id="wrapper">
-		<div class="content">
-			<div id="out-jubel">
-				<div class="photo-jubel"></div>
-				<div class="jubel-note">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</div>
-			</div>
-			<div id="out-jubel">
-				<div class="photo-jubel"></div>
-				<div class="jubel-note">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</div>
-			</div>
-			<div id="out-jubel">
-				<div class="photo-jubel"></div>
-				<div class="jubel-note">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</div>
-			</div>
+		<div id="baju1">
+			<div class="pakai1">gambar 1</div>
+			<div class="note">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>
 		</div>
-		<div class="rightKet">
-			<div id="arsip">
-				<div class="judul">Arsip</div>
-				<div class="isi-arsip">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</div>
-			</div>
+		<div id="baju2">
+			<div class="pakai1">gambar 1</div>
+			<div class="note">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+		</div>
+		<div id="baju3">
+			<div class="pakai1">gambar 1</div>
+			<div class="note">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 		</div>
 	</div>
 	<div id="footer">
