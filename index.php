b4fc82ae03859c37c1c1e7786d002d9daa5906d0
rina yulyani <rinayulyani12@gmail.com>
2016-02-23 15:24:24 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="assets/js/jquery-1.9.0.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.nivo.slider.js"></script>
+	<link href="assets/js/nivo-slider.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+	<link href="assets/js/default.css" rel="stylesheet" type="text/css" />
+	<title> Ukhti </title>
+	<script type="text/javascript">
+			$(window).load(function() {
+				$('#slider').nivoSlider();
+			});
+			</script>
+    <link rel="stylesheet" href="assets/images/themes/default/default.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/images/themes/light/light.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/images/themes/dark/dark.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/images/themes/bar/bar.css" type="text/css" media="screen" />
+</head>
+<body>
+	<div id="header">
+		<div class="log">
+			<a href="index.php"><div id="logo">Ukhti</div></a>
+			<div id="log">
+				<ul>
+					<li><a href="about.php">About</a></li>
+					<li><a href="logUp.php">Log Up</a></li>
+					<li><a href="logIn.php">Log In</a></li>
+					<li><a href="logOut.php">Log Out</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="nav">
+			<ul>
+				<div class="nav-in"><li><a href="jubel.php">Toko</a></li></div>
+				<div class="nav-in"><li><a href="kisah.php">Kisah</a></li></div>
+				<div class="nav-in"><li><a href="tutorial.php">Tutorial</a></li></div>
+				<div class="nav-in"><li><a href="hadits.php">Hadits</a></li></div>
+			</ul>
+		</div>
+	</div>
+	<div id="search">
+		<form>
+			<input type="text" name="" placeholder="Search"></input>
+			<input type="submit" name="search" value=""></input>
+		</form>
+	</div>
+	<div id="wrapper">
+		<div class="wrap">
+			<div class="content">
+				<div id="js">
+					<div class="slider-wrapper theme-default">
+						<div id="slider" class="nivoSlider">
+							<img src="assets/images/1.jpg">
+							<img src="assets/images/2.jpg">
+							<img src="assets/images/3.jpg">
+							<img src="assets/images/4.jpg">
+							<img src="assets/images/5.jpg">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="rightKet">
+				<div id="arsip">
+					<div class="judul">Arsip</div>
+					<div class="isi-arsip">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="contact">
+			<div id="logoF">Ukhti</div>
+			<a href="https://www.facebook.com/ukhtiwebsiteinformation/?ref=aymt_homepage_panel" target="_blank"><div id="kontak">Facebook</div></a>
+			<a href="https://twitter.com/Ukhti19f" target="_blank"><div id="kontak">Twitter</div></a>
+			<div id="kontak">ukhti19f16@gmail.com</div>
+			<div id="kontak">No. Kontak</div>
+			<div id="copyright">Ukhti &copy 2016</div>
+		</div>
+		<div class="info">
+			<div id="judul">Partner</div>
+			<div id="out-logo">
+				<div class="logoP"></div>
+				<div class="logoP"></div>
+				<div class="logoP"></div>
+				<div class="logoP"></div>
+				<div class="logoP"></div>
+				<div class="logoP"></div>
+				<div class="logoP"></div>
+				<div class="logoP"></div>
+			</div>
+		</div>
+		<div class="info">
+			<div id="judul">About Ukhti</div>
+			<div id="aboutringkasan">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+	</div>
+</body>
+</html>
