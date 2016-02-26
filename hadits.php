dce8c6ac14100a18fe0e3e96803c13dec9b26be1
Shania14 <shania.indriani14@gmail.com>
2016-02-26 09:42:50 +0700

    Udah ditambah keterangan di bagian hadits.php ^^

--- hadits.php
+++ hadits.php
@@ -89,6 +89,12 @@
 				</div>
 				<div class="rightKet">
 					<div id="arsip">
+					 <div class="judul">Keterangan</div>
+					 <div class="isi-ket"> Assalamualaikum Ukhti ^^ Laman ini berisi hadits shahih tentang wanita. Semoga Bermanfaat^^</div>
+					</div>
+				</div>
+				<div class="rightKet">
+					<div id="arsip">
 						<div class="judul">Arsip</div>
 						<div class="isi-arsip">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
