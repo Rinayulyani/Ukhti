5647b68bcaa92ea7b82613333014698c5face1f1
rina yulyani <rinayulyani12@gmail.com>
2016-02-29 12:07:41 +0700

    admin.php

---
+++ admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Handlee' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div id="wrapper">
+		<div class="out-admin">
+			<div id="box-atas">
+				<ul>
+					<li><a href="postingan.php">Postingan</a></li>
+					<li><a href="user.php">User</a></li>
+					<li><a href="pengunjung.php">Pengunjung</a></li>
+				</ul>
+			</div>
+			<div id="box-bawah"></div>
+		</div>
+	</div>
+</body>
+</html>
