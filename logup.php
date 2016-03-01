322c81d389cd72d0bb8e84dc41b24c591b100291
rina yulyani <rinayulyani12@gmail.com>
2016-03-01 09:33:33 +0700

    log.up

--- logup.php
+++ logup.php
@@ -10,11 +10,11 @@
 		<div class="out-logup">
 			<div class="out-logup1">
 				<div id="daftar-logup">
-					<div class="cinta">Nama Depan</div>
-					<div class="cinta">Nama Belakang</div>
+					<div class="cinta">Nama Lengkap</div>
 					<div class="cinta">Username</div>
 					<div class="cinta">Tempat tanggal lahir</div>
 					<div class="cinta">Jenis Kelamin</div>
+					<div class="cinta">Status</div>
 					<div class="cinta">Email</div>
 					<div class="cinta">Alternatif Email</div>
 					<div class="cinta">Password</div>
@@ -33,12 +33,16 @@
 				</div>
 				<div id="kolom-logup">
 					<form>
-						<input type="text" name="fnama" placeholder="Nama Depan" class="nama-logup"></input>
-						<input type="text" name="lnama" placeholder="Nama Belakang" class="nama-logup"></input>
+						<input type="text" name="fnama" placeholder="Nama Lengkap" class="nama-logup"></input>
 						<input type="text" name="username" placeholder="Username" class="nama-logup"></input>
 						<input type="text" name="tempat" placeholder="Tempat" class="tempat"></input>
 						<input type="date" name="tgllahir"  class="date"></input>
-						<input type="text" name="jk" placeholder="Jenis Kelamin" class="nama-logup1"></input>
+						<input type="radio" name="jk" value="Perempuan" class="radiojk">Perempuan</input>
+						<input type="radio" name="jk" value="Laki-laki" class="radiojk">Laki-laki</input>
+						<select name="status" class="nama-logup1">
+							<option value="lajang">Lajang</option>
+							<option value="menikah">Menikah</option>
+						</select>
 						<input type="email" name="email" placeholder="Email" class="nama-logup1"></input>
 						<input type="email" name="alternatifemail" placeholder="Alternatif Email" class="nama-logup1"></input>
 						<input type="password" name="password" placeholder="Password" class="nama-logup1"></input>
